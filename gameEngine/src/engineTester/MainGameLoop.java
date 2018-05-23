c8d29ea1dbcbdc9db2eb12d363f42309f283ad3f
ProSynth <31670455+ProSynth@users.noreply.github.com>
2018-05-23 16:30:02 +0200

    Módosítások

--- gameEngine/src/engineTester/MainGameLoop.java
+++ gameEngine/src/engineTester/MainGameLoop.java
@@ -58,15 +58,15 @@ public class MainGameLoop {
 		String pr = "Próba string jávában";
 		System.out.println("A string mérete:" + pr.getBytes().length);
 		
-		if (net != null)
-			net.disconnect();
-		net = new DeLoreanServerStateMachine(State.DISCONNECTED);
-		net.connect("192.168.1.104");
-		
 //		if (net != null)
 //			net.disconnect();
-//		net = new DeLoreanClientStateMachine();
-//		net.connect("localhost");
+//		net = new DeLoreanServerStateMachine(State.DISCONNECTED);
+//		net.connect("192.168.1.104");
+		
+		if (net != null)
+			net.disconnect();
+		net = new DeLoreanClientStateMachine("192.168.1.105");
+		net.connect("192.168.1.105");
 
 		/*
 		
