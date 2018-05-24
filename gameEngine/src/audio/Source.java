443c8d7f42ea2a06b9cc792f84d1dde1f40027b1
Biró Adrienn <biroadrienn@Biros-iMac.local>
2018-05-24 18:11:27 +0200

    audio hozzáadva

--- gameEngine/src/audio/Source.java
+++ gameEngine/src/audio/Source.java
@@ -8,9 +8,9 @@ public class Source {
 	
 	public Source() {
 		sourceId = AL10.alGenSources();
-		AL10.alSourcef(sourceId, AL10.AL_GAIN, 1);
-		AL10.alSourcef(sourceId, AL10.AL_PITCH, 1);
-		AL10.alSource3f(sourceId, AL10.AL_POSITION, 0, 0, 0);
+		
+		AL10.alSourcei(sourceId, AL10.AL_LOOPING, 1);
+		
 
 	}
 
@@ -19,12 +19,25 @@ public class Source {
 		AL10.alSourcePlay(sourceId);
 	}
 	
+	public void setVolume(float volume) {
+		AL10.alSourcef(sourceId, AL10.AL_GAIN, volume);
+	}
+	
+	public void SetPosition(float x, float y, float z) {
+		AL10.alSource3f(sourceId, AL10.AL_POSITION, x, y, z);
+	}	
+		
+	public void SetListener(float x, float y, float z) {
+		AL10.alListener3f(AL10.AL_POSITION, x, y, z);
+	}
 	
 	public void delete() {
 		AL10.alDeleteSources(sourceId);
 	}	
 	
-	
+	public void EnginePitch(float pitch) {		//max speed: 
+		AL10.alSourcef(sourceId, AL10.AL_PITCH, pitch);
+	}
 	
 	
 }
